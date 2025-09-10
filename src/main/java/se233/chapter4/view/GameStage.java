83bbd63fbd68ad7b9c5ee123bce994edb9e81951
ไอ่ปุณณ์ <Punnlaowatchara@gmail.com>
2025-09-11 01:44:07 +0700

    Before error rockman stand still

--- src/main/java/se233/chapter4/view/GameStage.java
+++ src/main/java/se233/chapter4/view/GameStage.java
@@ -17,12 +17,31 @@ public class GameStage extends Pane {
     private Keys keys;
     public GameStage () {
         keys = new Keys();
-        gameStageImg = new Image(Launcher. class.getResourceAsStream("assets/Background.png"));
+        gameStageImg = new Image(Launcher.class.getResourceAsStream("assets/Background.png"));
         ImageView backgroundImg = new ImageView(gameStageImg);
         backgroundImg.setFitHeight(HEIGHT);
         backgroundImg.setFitWidth(WIDTH);
-        gameCharacter = new GameCharacter(30, 30,0,0, KeyCode.A,KeyCode.D,KeyCode.W);
-        getChildren().addAll(backgroundImg, gameCharacter);
+
+//        gameCharacter = new GameCharacter(30, 30,0,0, KeyCode.A,KeyCode.D,KeyCode.W);
+//        getChildren().addAll(backgroundImg, gameCharacter);
+//
+//        GameCharacter character2 = new GameCharacter(30, 30, 0, 0, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP);
+//        character2.xMaxVelocity = 10;
+//        character2.yMaxVelocity = 20;
+//        character2.getImageView().setImage(new Image(Launcher.class.getResourceAsStream("assets/rockman.png")));
+//        getChildren().add(character2);
+
+        GameCharacter mario = new GameCharacter(
+                30, 30, 0, 0, "assets/MarioSheet.png", 4, 4, 1, 16, 32, KeyCode.A, KeyCode.D, KeyCode.W
+        );
+
+        GameCharacter rockman = new GameCharacter(
+                150, 30, 0, 0, "assets/rockman.png", 10, 5, 2, 404, 384, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.UP
+        );
+
+        gameCharacter = mario;
+        getChildren().addAll(backgroundImg, mario, rockman);
+
     }
 
     public GameCharacter getGameCharacter() {
